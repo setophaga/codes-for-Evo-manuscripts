02e8cb22363ca6d195960867e0c670873705ecf2
Silu Wang <silu_wang@hotmail.com>
2015-01-19 13:23:04 -0800

    This R file is a bootstrap correlation function
    This is a function for bootstrap correlation: a and b are vectors to be
    tested for correlation, n is the size of bootstrap samples to be
    collected, this function will return 1), correlation coefficient of the
    two values; 2), standard error of correlation coefficeint of the
    bootstrap samples, and 3) p value that represents the probability of
    getting a correlation coeffient as large or larger than the correlation
    coefficent of our data in the bootstrap samples.

---
+++ bootstrap correlation test.R
@@ -0,0 +1,18 @@
+bootstrap_corr<-function(a, b, n)
+{r.boot<-{}
+for (i in 1:n)
+	{a.s<-sample(a, length(a), replace=T)
+	b.s<-sample(b, length(b), replace=T)
+	if(sd(a.s)==0||sd(b.s)==0)
+		{r1<-0}
+	else {r1<-cov(a.s,b.s)/(sd(a.s)*sd(b.s))}
+	r.boot<-c(r.boot,r1)}
+	r<-cov(a,b)/(sd(a)*sd(b))
+	m<-0
+for (i in 1: n)
+	{if(r.boot[[i]]>r||r.boot[[i]]==r)
+		{m<-m+1}}
+p<-m/n
+se.rboot<-sd(r.boot)/sqrt(n)
+return(c(r, se.rboot, p))
+}
